21232f8b25b0ef5292050e144302053ead4c8bf2
Lauren Murphy <Lmurphy@gmx.com>
2022-03-08 15:54:43 -0800

    Added props, state, userPrefs handling

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import PinballForm from './PinballForm.js';
 import ResultsDisplay from './ResultsDisplay';
+import '../main.css';
+import axios from 'axios';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pinballResults: [],
+      restaurantResults: [], //Returned list of nearby restaurants relevant to selected pinball machine
+      userPreferences: {} //These are our filter criteria.  They are saved to mongo so they can be reloaded next session
+    }
+  }
+
+  setUserPreferences = (userPreferences) => {
+    this.setState({ userPreferences });
+  }
+
+  //This is what talks to our backend to get our response back
+  getPinballResults = async () => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/pinball?searchQuery=${this.state.userPreferences.location}`;
+      let pinballResults = await axios.get(url);
+      this.setState({ pinballResults: pinballResults.data });
+    } catch (error) {
+      console.log("Error in getPinballResults", error.message);
+    }
+  }
 
   render() {
+    console.log(this.state.userPreferences);
     return (
       <>
-      <h1>Pinball Date</h1>
-        <PinballForm />
-        <ResultsDisplay />
+        <h1>Pinball Date</h1>
+        <PinballForm
+          setUserPreferences={this.setUserPreferences}
+          getPinballResults={this.getPinballResults}
+        />
+        <ResultsDisplay
+          pinballResults={this.state.pinballResults}
+          userPreferences={this.state.userPreferences}
+          restaurantResults={this.state.restaurantResults}
+        />
       </>
     );
   }
