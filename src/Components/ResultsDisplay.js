94ba899184ba7227617c99b10c8c222f09fd5fc0
Lauren Murphy <Lmurphy@gmx.com>
2022-03-08 15:55:26 -0800

    Filled out accordion items (not dynamic)

--- src/Components/ResultsDisplay.js
+++ src/Components/ResultsDisplay.js
@@ -7,26 +7,53 @@ class ResultsDisplay extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedPinball: []
+      selectedPinball: [],
+      pinballResults: [],
+      userPreferences: []
     }
   }
 
+  //In props: pinballResults, userPreferences
+
   render() {
-
-    let pinballResults = [];
-
 
     return (
       <>
-        <Container className = "results">
-          <Accordion id="pinballResults">
-            <Accordion.Item>
-              sdfsdfsdfsdf
+        <Container className="results">
+          <Accordion id="pinballResults" className="w-50 m-3 p-2">
+
+            {/* these will be dynamically generated using array.map */}
+            <Accordion.Item key="i">
+              <Accordion.Header>
+                Title Text Goes here
+              </Accordion.Header>
+              <Accordion.Body>
+                <img src="https://place-hold.it/200x200" alt="Pinball"></img>
+                Body content goes here
+                <ul>
+                  <li>Machine 1</li>
+                  <li>Machine 2</li>
+                  <li>Machine 3</li>
+                </ul>
+              </Accordion.Body>
             </Accordion.Item>
+
           </Accordion>
-          <Accordion id="restaurantResults">
-            <Accordion.Item>
-              sdfsdfsdfsdf
+          <Accordion id="restaurantResults" className="w-50 m-3 p-2">
+            {/* these will be dynamically generated using array.map */}
+            <Accordion.Item key="i">
+              <Accordion.Header>
+                Title Text Goes here
+              </Accordion.Header>
+              <Accordion.Body>
+                Body content goes here
+                <ul>
+                  <li>Address</li>
+                  <li>Hours</li>
+                  <li>Rating</li>
+                </ul>
+                <img src="https://place-hold.it/200x200" alt="Pinball"></img>
+              </Accordion.Body>
             </Accordion.Item>
           </Accordion>
         </Container>
