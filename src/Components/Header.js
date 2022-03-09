a32625140d3d33a0a6edf7b49257866b1bca6aad
Lauren Murphy <Lmurphy@gmx.com>
2022-03-09 13:24:51 -0800

    Implemented auth0

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button'
+import LoginButton from './LoginButton.js'
+import LogoutButton from './LogoutButton.js'
 import '../header.css';
+import { withAuth0 } from '@auth0/auth0-react';
 
 class Header extends React.Component {
 
@@ -9,16 +12,18 @@ class Header extends React.Component {
       <div className="header">
         <p>Pinball Date</p>
         <div className="navButtons">
-          {/* Log-in: */}
-         <Button>Log-in</Button>
-          {/* Log-out: */}
-          <Button>Log-out</Button>
-          {/* Profile: */}
-          <Button>Profile</Button>
+          {/* If user not logged in, show Login button: */}
+          {!this.props.auth0.user && <LoginButton />}
+
+          {/* If user logged in, show Log-out button and profile: */}
+          {this.props.auth0.user && <>
+            <LogoutButton />
+            <Button>Profile</Button>
+          </>}
         </div>
       </div>
     );
   }
 }
 
-export default Header;
+export default withAuth0(Header);
