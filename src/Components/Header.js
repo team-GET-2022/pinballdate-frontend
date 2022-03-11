f404319cced3d76090325edb89295463504990bb
Lauren Murphy <Lmurphy@gmx.com>
2022-03-10 22:03:58 -0800

    Updated header elements

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,6 @@ import LoginButton from './LoginButton.js'
 import LogoutButton from './LogoutButton.js'
 import '../header.css';
 import { withAuth0 } from '@auth0/auth0-react';
-import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
@@ -12,7 +11,7 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <p>Pinball Date</p>
+        {/* <p>Pinball Date</p> */}
         <div className="navButtons">
           {/* If user not logged in, show Login button: */}
           {!this.props.auth0.user && <LoginButton postNewUser={this.props.postNewUser} />}
@@ -20,8 +19,7 @@ class Header extends React.Component {
           {/* If user logged in, show Log-out button and profile: */}
           {this.props.auth0.user && <>
             <LogoutButton />
-            <Nav.Item><Link to="/" className='nav-link'><Button>Home Page</Button></Link> </Nav.Item>
-            <Nav.Item><Link to="/favorites" className='nav-link'><Button>Favorites</Button></Link> </Nav.Item>
+            <Link to="/favorites" className='nav-link'><Button>Favorites</Button></Link>
           </>}
           <Button onClick={()=>this.props.toggleAboutUsModal()}>About Us</Button>
         </div>
